e30f10daf6f3ffcba922deab50c315230287f90e
MarkusJx <28785953+markusjx@users.noreply.github.com>
2020-10-01 20:27:07 +0200

    Update ui; Use transform_graph to improve the model size
    Signed-off-by: MarkusJx <28785953+MarkusJx@users.noreply.github.com>

--- ui/main.js
+++ ui/main.js
@@ -84,19 +84,19 @@ function makeSumsDisplayable(sum, k = false) {
     let negative = sum < 0;
     sum = Math.abs(sum);
 
-    if (sum > 1000000000) {
+    if (sum > 1000000000) { // One billion
         if (negative) {
             return ((Math.round(sum / 100000000) / 10) * (-1)) + "B";
         } else {
             return Math.round(sum / 100000000) / 10 + "B";
         }
-    } else if (sum > 1000000) {
+    } else if (sum > 1000000) { // One million
         if (negative) {
             return ((Math.round(sum / 100000) / 10) * (-1)) + "M";
         } else {
             return Math.round(sum / 100000) / 10 + "M";
         }
-    } else if (k && sum > 1000) {
+    } else if (k && sum > 1000) { // One thousand
         if (negative) {
             return ((Math.round(sum / 100) / 10) * (-1)) + "K";
         } else {
@@ -126,11 +126,11 @@ function addMoney(value) {
 autobetLib.callbacks.setAllMoneyMadeCallback(setAllMoneyMade);
 
 function setAllMoneyMade(value) {
-    moneyall.innerHTML = makeSumsDisplayable(value) + " $";
+    moneyall.innerHTML = "$" + makeSumsDisplayable(value);
 }
 
 function updateValues() {
-    moneythishour.innerHTML = makeSumsDisplayable(getMoneyPerHour(), true) + " $/hr";
+    moneythishour.innerHTML = "$" + makeSumsDisplayable(getMoneyPerHour(), true) + "/hr";
     raceswon.innerHTML = won;
     winprobability.innerHTML = Math.round((won / (won + lost)) * 1000) / 10 + "%";
 }
@@ -164,23 +164,27 @@ function setQRCode(ip) {
     });
 }
 
+// set the ips
 function setIPs() {
     let ip = autobetLib.getIP();
     weblink.innerHTML = "http://" + ip + ":8027";
     setQRCode(ip);
 }
 
+// Open the web page when the web link is clicked
 weblink.addEventListener('click', () => {
     autobetLib.openWebsite();
 });
 
+// Listen for change on the enable webserver switch
 enable_webserver.listen('change', () => {
     enable_webserver.disabled = true;
     autobetLib.settings.setWebServer(enable_webserver.checked).then((res) => {
-        if (!res) {
+        if (!res) { // If the call failed, set the switch to the web servers current state
             enable_webserver.checked = autobetLib.settings.webServerRunning();
         }
 
+        // Disable the buttons for the web server or enable them
         if (enable_webserver.checked) {
             weblink.disabled = false;
             showqrbutton.disabled = false;
@@ -191,6 +195,7 @@ enable_webserver.listen('change', () => {
             showqrbutton.disabled = true;
         }
 
+        // Save the settings
         autobetLib.settings.saveSettings().then(() => {
             enable_webserver.disabled = false;
             settings_saved_msg.close();
@@ -199,6 +204,7 @@ enable_webserver.listen('change', () => {
     });
 });
 
+// when the window unloads, quit electron
 window.onbeforeunload = function() {
     autobetLib.shutdown().then(() => {
         electron.quit();
@@ -226,14 +232,16 @@ async function main() {
 
     await autobetLib.loadWinnings();
 
-    // Set swtches checkes
+    // Set switches checked
     time_sleep_field.value = autobetLib.settings.getTimeSleep();
     log_to_file_switch.checked = autobetLib.logging.isLoggingToFile();
     log_to_console_switch.checked = autobetLib.logging.isLoggingToConsole();
+    // Resize the "console" if logging to console is enabled
     if (autobetLib.logging.isLoggingToConsole()) {
         log_textfield_resizer.style.height = "178px";
     }
 
+    // Check the web server activated and start it if activated
     enable_webserver.checked = autobetLib.settings.webServerActivated();
     if (autobetLib.settings.webServerActivated()) {
         initialized = await autobetLib.startWebServer();
@@ -262,6 +270,7 @@ async function main() {
 main().then(() => {
     console.log("Main finished.");
 }, () => {
+    // main failed
     errordialog.open();
     errordialog.listen("MDCDialog:closed", function() {
         autobetLib.shutdown();
@@ -294,12 +303,18 @@ function showDescription(title, description) {
     description_dialog.open();
 }
 
+// Listen for keyup events on the input of the time_sleep text field
 time_sleep_field.input_.addEventListener('keyup', (event) => {
+    // Only do this if the key pressed was 'enter'
     if (event.keyCode === 13) {
         event.preventDefault();
+        // If the length of the value string is not zero,
+        // save the settings
         if (time_sleep_field.value.length !== 0) {
             autobetLib.settings.setTimeSleep(Number(time_sleep_field.value));
             time_sleep_field.disabled = true;
+
+            // Save the settings
             autobetLib.settings.saveSettings().then(() => {
                 time_sleep_field.disabled = false;
                 settings_saved_msg.close();
@@ -309,11 +324,17 @@ time_sleep_field.input_.addEventListener('keyup', (event) => {
     }
 });
 
+// Listen for change on the switch that toggles full debugging
 full_debug.listen('change', () => {
     full_debug.disabled = true;
     if (full_debug.checked) {
         log_to_file_switch.checked = true;
         log_to_file_switch.disabled = true;
+
+        // If we are not already logging to file, enable that.
+        // Or enable at least that switch so the user knows its enabled.
+        // Also, prevent the user from switching that switch, as
+        // logging to file is a essential part of debug:full
         if (!autobetLib.logging.isLoggingToFile()) {
             autobetLib.logging.setLogToFile(true);
             autobetLib.settings.saveSettings().then(() => {
@@ -325,8 +346,9 @@ full_debug.listen('change', () => {
         log_to_file_switch.disabled = false;
     }
 
+    // Set debug:full
     autobetLib.settings.setDebugFull(full_debug.checked).then((res) => {
-        if (!res) {
+        if (!res) { // If the call failed, do some stuff
             full_debug.checked = false;
             console.warn("setDebugFull returned false");
         }
@@ -334,9 +356,12 @@ full_debug.listen('change', () => {
     });
 });
 
+// Listen for change on the switch that toggles logging to file
 log_to_file_switch.listen('change', () => {
     log_to_file_switch.disabled = true;
     autobetLib.logging.setLogToFile(log_to_file_switch.checked);
+
+    // save settings
     autobetLib.settings.saveSettings().then(() => {
         log_to_file_switch.disabled = false;
         settings_saved_msg.close();
@@ -344,13 +369,23 @@ log_to_file_switch.listen('change', () => {
     });
 });
 
+// Listen for change on the switch that toggles logging to "console"
 log_to_console_switch.listen('change', () => {
     log_to_console_switch.disabled = true;
     autobetLib.logging.setLogToConsole(log_to_console_switch.checked);
 
     if (!log_to_console_switch.checked) {
+        // Empty the text field and set its size to its default.
         log_textfield.value = "";
         log_textfield_resizer.style.height = "56px";
+
+        // Do the exact thing again. All these calls are async so it can still
+        // be written to the "console" even though logging is disabled.
+        // Jank solutions require more janky solutions as a response.
+        setTimeout(() => {
+            log_textfield.value = "";
+            log_textfield_resizer.style.height = "56px";
+        }, 500);
 
         // Animate the resize process
         log_textfield_resizer.className = "mdc-text-field__resizer decrease-height";
@@ -364,6 +399,8 @@ log_to_console_switch.listen('change', () => {
             log_textfield_resizer.style.height = "178px";
         }, 500);
     }
+
+    // save settings
     autobetLib.settings.saveSettings().then(() => {
         log_to_console_switch.disabled = false;
         settings_saved_msg.close();
@@ -371,18 +408,26 @@ log_to_console_switch.listen('change', () => {
     });
 });
 
+// Set a callback for logging to "console"
 autobetLib.logging.setLogCallback((msg) => {
+    // Only do this if the text field is scrolled all the way down
     if ((log_textfield.input_.scrollTop + log_textfield.input_.clientHeight) >= (log_textfield.input_.scrollHeight - 20)) {
+        // Append the message
         log_textfield.value += msg;
+
+        // The text field was scrolled down, scroll all the way down,
+        // so new messages will always be on the bottom of the text field
         log_textfield.input_.scroll({
             top: log_textfield.input_.scrollHeight,
             left: 0
         });
     } else {
+        // The text field is not scrolled all the way down, just append the message
         log_textfield.value += msg;
     }
 });
 
+// Add some info texts
 document.getElementById("time-sleep-info").addEventListener('click', () => {
     showDescription("Time-sleep", "Set the time to sleep after a bet has started. Use this option, when the program did not immediately start a new bet when the " +
         "race has finished. Press enter to save, the default value is 36.");
