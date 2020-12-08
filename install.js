d38ae31e2d92378d8cfb8ebfe23082ab3f3c93db
MarkusJx <28785953+MarkusJx@users.noreply.github.com>
2020-12-08 14:53:43 +0100

    Download model using https

--- install.js
+++ install.js
@@ -2,38 +2,124 @@
 
 const fs = require('fs');
 const path = require('path');
-const request = require('request');
+const https = require('https');
 
-// Source: https://stackoverflow.com/a/32134846
-const download = (url, dest) => {
+/**
+ * Source: https://medium.com/@bargord11/write-your-first-node-js-terminal-progress-bar-5bd5edb8a563
+ */
+class ProgressBar {
+    constructor() {
+        this.total;
+        this.current;
+        this.bar_length = process.stdout.columns - 30;
+    }
+
+    init(total) {
+        this.total = total;
+        this.current = 0;
+        this.update(this.current);
+    }
+
+    update(current) {
+        this.current = current;
+        const current_progress = this.current / this.total;
+        this.draw(current_progress);
+    }
+
+    draw(current_progress) {
+        if (this.finished) return;
+        const filled_bar_length = (current_progress * this.bar_length).toFixed(0);
+        const empty_bar_length = this.bar_length - filled_bar_length;
+
+        const filled_bar = this.get_bar(filled_bar_length, "█", a => `\x1b[37m${a}\x1b[0m`);
+        const empty_bar = this.get_bar(empty_bar_length, "░", a => `\x1b[37m${a}\x1b[0m`);
+        const percentage_progress = (current_progress * 100).toFixed(2);
+
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(
+            `\x1b[35mProgress:\x1b[0m |${filled_bar}${empty_bar}| ${percentage_progress}%`
+        );
+    }
+
+    get_bar(length, char, color = a => a) {
+        let str = "";
+        for (let i = 0; i < length; i++) {
+            str += char;
+        }
+        return color(str);
+    }
+};
+
+/**
+ * Download a file
+ * 
+ * @param {string} url the url of the file to download
+ * @param {string} filePath the output path of the file
+ * @param {fs.WriteStream} file the file stream
+ */
+async function download(url, filePath, file = null) {
     return new Promise((resolve, reject) => {
-        const file = fs.createWriteStream(dest);
-        const sendReq = request.get(url);
+        let fileInfo = null, closeFile = true;
+        if (file == null) file = fs.createWriteStream(filePath);
+        else closeFile = false;
 
-        // verify response code
-        sendReq.on('response', (response) => {
-            if (response.statusCode !== 200) {
-                return reject('Response status was ' + response.statusCode);
+        const request = https.get(url, response => {
+            if (response.statusCode >= 300 && response.statusCode < 400 && response.headers.location != undefined) {
+                if (response.headers.location.startsWith("https://")) {
+                    url = response.headers.location;
+                } else {
+                    const addr_regex = /^https:\/\/(www\.)?[a-zA-Z0-9\.]+\.[a-z]{1,5}/;
+                    url = url.match(addr_regex)[0] + response.headers.location;
+                }
+
+                console.log(`\x1b[2mRedirecting to: ${url}\x1b[0m`);
+
+                return download(url, filePath, file).then(resolve, reject);
+            } else if (response.statusCode !== 200) {
+                reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
+                return;
             }
 
-            sendReq.pipe(file);
+            fileInfo = {
+                mime: response.headers['content-type'],
+                size: parseInt(response.headers['content-length'], 10),
+            };
+
+            const bar = new ProgressBar();
+            bar.init(fileInfo.size);
+
+            let current = 0;
+            response.on('data', chunk => {
+                current += chunk.length;
+                bar.update(current);
+                file.write(chunk);
+
+                if (current >= fileInfo.size) {
+                    console.log();
+                    file.end();
+                }
+            });
         });
 
-        // close() is async, call cb after close completes
-        file.on('finish', () => file.close(resolve));
+        if (closeFile) {
+            // The destination stream is ended by the time it's called
+            file.on('finish', () => {
+                resolve(fileInfo);
+            });
 
-        // check for request errors
-        sendReq.on('error', (err) => {
-            fs.unlink(dest);
-            return reject(err.message);
+            file.on('error', err => {
+                fs.unlink(filePath, () => reject(err));
+            });
+        }
+
+        request.on('error', err => {
+            fs.unlink(filePath, () => reject(err));
         });
 
-        file.on('error', (err) => { // Handle errors
-            fs.unlink(dest); // Delete the file async. (But we don't check the result)
-            return reject(err.message);
-        });
+        request.end();
     });
-};
+}
 
 // Source: https://stackoverflow.com/a/32197381
 const deleteFolderRecursive = function (p) {
@@ -103,7 +189,7 @@ async function downloadModel() {
 
     console.log("Downloading model.yml");
     await download(dl_addr, modelPath);
-    console.log("Successfully downloaded the model");
+    console.log("\x1b[92mSuccessfully downloaded the model\x1b[0m");
 }
 
 if (process.argv.length !== 3) {
