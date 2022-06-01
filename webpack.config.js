0cde562781b05c9fd6f737730d155e191934b260
MarkusJx <28785953+markusjx@users.noreply.github.com>
2022-06-01 10:59:45 +0200

    Package using webpack
    Signed-off-by: Markus <28785953+MarkusJx@users.noreply.github.com>

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = [
+    {
+        entry: './electron-src/main/index.ts',
+        target: 'electron-main',
+        externalsPresets: {
+            node: true,
+        },
+        externals: [nodeExternals()],
+        output: {
+            path: path.join(__dirname, 'out'),
+            filename: 'index.bundled.js',
+        },
+        node: {
+            __dirname: false,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.node$/,
+                    loader: 'node-loader',
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.ts'],
+        },
+    },
+    {
+        entry: './electron-src/preload/preload.ts',
+        target: 'electron-preload',
+        externalsPresets: {
+            node: true,
+        },
+        externals: [nodeExternals()],
+        output: {
+            path: path.join(__dirname, 'out'),
+            filename: 'preload.bundled.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.node$/,
+                    loader: 'node-loader',
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.ts'],
+        },
+    },
+];
